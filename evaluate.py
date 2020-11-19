234b03dca827e3f0b7291ea1eff2edc2d77b71db
Matthieu Futeral-Peter <matthieufuteral-peter@macbook-pro-de-matthieu.home>
2020-11-19 01:06:18 +0100

    update

--- evaluate.py
+++ evaluate.py
@@ -41,10 +41,10 @@ model = load_model(path_model=path_model,
 model.eval()
 
 # Transform data
-try:
-    data_transforms_dev = data_transformation(model=args.model, size=args.size, train=0)
-except:
-    raise NameError('model not defined')
+data_transforms_dev = data_transformation(model=args.model, size=args.size, train=0)
+data_reverse = transforms.Compose([transforms.RandomHorizontalFlip(1.), transforms.Resize((224, 224)),
+                                   transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                               std=[0.229, 0.224, 0.225])])
 
 test_dir = args.data + '/test_images/mistery_category'
 
@@ -60,6 +60,7 @@ output_file = open(path_output, "w")
 output_file.write("Id,Category\n")
 
 test_images = [img for img in os.listdir(test_dir) if 'jpg' in img]
+scores = {}
 doubles = {}
 
 for f in tqdm(test_images):
@@ -68,11 +69,21 @@ for f in tqdm(test_images):
         doubles[f.split('_')[0]] = []
 
     data = data_transforms_dev(pil_loader(test_dir + '/' + f))
+    reverse_data = data_reverse(pil_loader(test_dir + '/' + f))
     data = data.unsqueeze(0)
+    reverse_data = reverse_data.unsqueeze(0)
     if use_cuda:
         data = data.cuda()
-    output = model(data)
-    pred = output.data.max(1, keepdim=True)[1]
+        reverse_data = reverse_data.cuda()
+
+    output = F.softmax(model(data), dim=-1)
+    reverse_output = F.softmax(model(reverse_data), dim=-1)
+    pred = torch.argmax(output, dim=-1)       # output.data.max(1, keepdim=True)[1]
+    reverse_pred = torch.argmax(reverse_output, dim=-1)
+
+    if torch.max(output, dim=-1).item() < torch.max(reverse_output, dim=-1):
+        pred = reverse_pred
+        output = reverse_output
 
     if '_' in f:
         doubles[f.split('_')[0]].append((pred.item(), torch.max(output).data.item()))
@@ -80,6 +91,7 @@ for f in tqdm(test_images):
         output_file.write("%s,%d\n" % (f[:-4], pred))
 
 # pdb.set_trace()
+
 for k in doubles.keys():
     proba = [p[1] for p in doubles[k]]
     pred = doubles[k][np.argmax(proba)][0]
